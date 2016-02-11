9e6f1b42720d657eb32df9e5686185bc94d551e5
JiaweiShi <jiaweishi@Jiaweis-MacBook-Pro.local>
2016-02-10 22:44:19 -0800

    Initial check in

---
+++ app/src/main/java/com/example/jiaweishi/articlesearch/Article.java
@@ -0,0 +1,11 @@
+package com.example.jiaweishi.articlesearch;
+
+/**
+ * Created by jiaweishi on 2/10/16.
+ */
+public class Article {
+    private String headline;
+    private String imageLink;
+
+
+}
